2d7f76457549bc448aca725a19f14e7de50dd350
DerekMalone <laxmalone@gmail.com>
2021-08-25 20:53:37 -0500

    resolving an issue with Authors list not rendering to Books

--- src/javascripts/components/forms/selectAuthor.js
+++ src/javascripts/components/forms/selectAuthor.js
@@ -1,11 +1,11 @@
-import getAuthors from '../../helpers/data/authorData';
+import { getAuthor } from '../../helpers/data/authorData';
 
 const selectAuthor = () => {
   let domString = `<label for="author">Select an Author</label>
     <select class="form-control" id="author_id" required>
     <option value="">Select an Author</option>`;
 
-  getAuthors().then((authorsArray) => {
+  getAuthor().then((authorsArray) => {
     authorsArray.forEach((author) => {
       domString += `<option value="${author.firebaseKey}">${author.first_name} ${author.last_name}</option>`;
     });
